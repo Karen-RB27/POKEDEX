650b7f3bac6e77b88a727b9f88c811f1dc42ec88
Karen <rochabrigagaokaren@gmail.com>
2026-06-28 19:40:39 -0300

    commit

--- Pokeapp/services/favoritesService.ts
+++ Pokeapp/services/favoritesService.ts
@@ -1,6 +1,14 @@
-import { addDoc, collection } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
 import { db } from "./firebase";
 
+// CREATE
 export async function salvarFavorito(pokemon: any) {
   try {
     await addDoc(collection(db, "favoritos"), {
@@ -14,7 +22,36 @@ export async function salvarFavorito(pokemon: any) {
 
     return true;
   } catch (error) {
-    console.log(error);
+    console.log("Erro ao salvar:", error);
     return false;
   }
+}
+
+// READ
+export async function listarFavoritos() {
+  const snapshot = await getDocs(collection(db, "favoritos"));
+
+  return snapshot.docs.map((doc) => ({
+    idFirebase: doc.id,
+    ...doc.data(),
+  }));
+}
+
+// UPDATE
+export async function atualizarApelido(
+  idFirebase: string,
+  apelido: string
+) {
+  const referencia = doc(db, "favoritos", idFirebase);
+
+  await updateDoc(referencia, {
+    apelido,
+  });
+}
+
+// DELETE
+export async function excluirFavorito(idFirebase: string) {
+  const referencia = doc(db, "favoritos", idFirebase);
+
+  await deleteDoc(referencia);
 }
