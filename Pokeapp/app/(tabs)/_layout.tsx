002ef44f39aacca309e5c816e12d36a8b3f63277
Karen <rochabrigagaokaren@gmail.com>
2026-06-28 18:47:15 -0300

    commit

--- Pokeapp/app/(tabs)/_layout.tsx
+++ Pokeapp/app/(tabs)/_layout.tsx
@@ -23,13 +23,19 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+        <Tabs.Screen
+      name="favorites"
+      options={{
+      title: "Favoritos",
+      tabBarIcon: ({ color }) => (
+        <IconSymbol
+            size={28}
+            name="heart.fill"
+            color={color}
+            />
+        ),
+      }}
+  />
     </Tabs>
   );
 }
